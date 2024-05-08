ecb3be8a23aac68ac579df9e7b0362ea085d26c5
NicolasAntonioVieira <166947948+NicolasAntonioVieira@users.noreply.github.com>
2024-05-08 17:24:47 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+
+app = Flask(__name__)
+
+#Página inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+#Página de informação do grupo
+#@app.route('/sobre')
+#def sobreNos():
+ #   return render_template('sobre.html')
